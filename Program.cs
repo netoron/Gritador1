1ec90ad0be167de944b77aae253a79254b5930d6
netoron <61334943+netoron@users.noreply.github.com>
2020-05-15 23:50:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite algo em minusculo");
+            string conteudo = Console.ReadLine();
+            Console.WriteLine(conteudo.ToUpper());
+        }
+    }
+}
